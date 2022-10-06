94fb85a5e23aa229603c4b4e0e01d86add7ec814
Haloen <72333894+Haloen@users.noreply.github.com>
2022-10-06 15:59:19 +0800

    💞I LOVE JAVA🥰

---
+++ src/RCT.java
@@ -0,0 +1,5 @@
+
+
+public class RCT extends Object /*AVL*/ {
+
+}
---
+++ src/RCTTester.java
@@ -0,0 +1,5 @@
+public class RCTTester {
+    public static void main(String[] args) {
+        System.out.println("I Love JAVA");
+    }
+}
