5845b79d62cdb2bd53c01358641925b2328d1938
Haloen <72333894+Haloen@users.noreply.github.com>
2022-10-07 17:16:17 +0800

    💀I died💀
    I forgor to upload the AVL tree

---
+++ YX/AVLTester.java
@@ -0,0 +1,13 @@
+public class AVLTester {
+    public static void main(String[] args) {
+        Integer[] arr = {9, 5, 10, 0, 6, 11, -1, 1, 2};
+//        Integer[] arr = {1,2,3,4,5,6,10};
+        AVL<Integer> avl = new AVL<Integer>(arr);
+        System.out.println("Height of BST: " + avl.height(avl.getRoot()));
+        System.out.println(avl.preOrder());
+
+        avl.delete(10);
+        System.out.println(avl.preOrder());
+        System.out.println(arr[6].compareTo(10));
+    }
+}
