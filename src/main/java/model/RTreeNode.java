71d98de442ec620cc08e75e687c3ff63d0e22f5f
CSP-PC <csptpc@gmail.com>
2022-10-12 14:35:50 +0800

    isOverlap refactored

--- src/main/java/model/RTreeNode.java
+++ src/main/java/model/RTreeNode.java
@@ -10,38 +10,29 @@ public class RTreeNode<T> extends model.Node<T[]>{
     public boolean isLeaf() {return leaf;}
 
     // Ctors
-    public RTreeNode(T[] item) {
-        this(item, false);
+    public RTreeNode(T[] item, Pair<Double, Double>[] ranges) {
+        this(item, ranges, false);
     }
 
-    public RTreeNode(T[] item, RTreeNode<T>[] neighbours) {
-        this(item, false);
+    public RTreeNode(T[] item, Pair<Double, Double>[] ranges, RTreeNode<T>[] neighbours) {
+        this(item, ranges, false);
         this.neighbours[0] = neighbours[0];
         this.neighbours[1] = neighbours[1];
     }
 
-    public RTreeNode(T[] item, boolean leaf) {
+    public RTreeNode(T[] item, Pair<Double, Double>[] ranges, boolean leaf) {
         super(item);
         neighbours = new RTreeNode[2];
         this.leaf = leaf;
+        this.ranges = ranges;
     }
 
     static public boolean isOverlap( RTreeNode n1, RTreeNode n2 ) {
         if (n1.ranges.length != n2.ranges.length) throw new IllegalArgumentException("输入的顶点的范围数不一样");
 
         for (int i = 0; i < n1.ranges.length; i++) {
-            boolean overlapInThisDimension = false;
-
-            if (n1.ranges[i].getValue() == n2.ranges[i][0]) overlapInThisDimension = true;
-
-                else if (scoords[i] < coords[i])
-                    if (scoords[i] + sdimensions[i] >= coords[i]) overlapInThisDimension = true;
-
-                    else if (scoords[i] > coords[i])
-                        if ( coords[i] + dimensions[i] >= scoords[i] )
-                            overlapInThisDimension = true;
-
-            if (!overlapInThisDimension) return false;
+            if ((double) n1.ranges[i].getFirst() > (double) n2.ranges[i].getSecond() || (double) n2.ranges[i].getFirst() > (double) n1.ranges[i].getSecond())
+                return false;
         }
 
         return true;
