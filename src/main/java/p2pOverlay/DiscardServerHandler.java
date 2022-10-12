23d59ef753fd5baccbe5b9fe688b168b0c3b4528
justin lim <dosecomix@gmail.com>
2022-10-12 12:40:51 +0800

    Print received messages, success!

--- src/main/java/p2pOverlay/DiscardServerHandler.java
+++ src/main/java/p2pOverlay/DiscardServerHandler.java
@@ -1,5 +1,6 @@
 package p2pOverlay;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -11,6 +12,8 @@ public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         // discard
+        ByteBuf in = (ByteBuf) msg;
+        System.out.print(in.toString(io.netty.util.CharsetUtil.US_ASCII));
     }
 
     @Override
